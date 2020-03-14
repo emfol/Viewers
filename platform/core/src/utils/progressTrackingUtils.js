9cbd6f003243777b0a7571b8530de028cc12819b
Emanuel F. Oliveira <emfolg@gmail.com>
2020-03-14 12:58:52 -0300

    Feat #1130 Beta version of Progress Tracking utils

--- platform/core/src/utils/progressTrackingUtils.js
+++ platform/core/src/utils/progressTrackingUtils.js
@@ -13,9 +13,10 @@ const LIST = Symbol('List');
 function createTask() {
   return objectWithType(TASK, {
     next: null,
+    list: null,
     failed: false,
-    finished: false,
     awaiting: null,
+    progress: 0.0,
   });
 }
 
@@ -29,6 +30,7 @@ function createList() {
 function increaseList(list) {
   if (isList(list)) {
     const task = createTask();
+    task.list = list;
     if (isTask(list.head)) {
       task.next = list.head;
     }
@@ -46,21 +48,47 @@ function isList(subject) {
   return isOfType(LIST, subject);
 }
 
-function progress(list) {
+function update(task, value) {
+  if (isTask(task) && isValidProgress(value) && value < 1.0) {
+    const { progress } = task;
+    if (value !== progress) {
+      const { list } = task;
+      task.progress = value;
+      if (isList(list)) {
+        notify(list, getOverallProgress(list));
+      }
+    }
+  }
+}
+
+function finish(task) {
+  if (isTask(task)) {
+    const { list } = task;
+    task.progress = 1.0;
+    task.awaiting = null;
+    Object.freeze(task);
+    if (isList(list)) {
+      notify(list, getOverallProgress(list));
+    }
+  }
+}
+
+function getOverallProgress(list) {
   const status = createStatus();
   if (isList(list) && isTask(list.head)) {
     let item = list.head;
     do {
-      ++status.total;
-      if (item.finished) {
-        ++status.finished;
-        if (item.failed) ++status.failed;
+      const { progress } = item;
+      status.total++;
+      if (isValidProgress(progress)) {
+        status.partial += progress;
+        if (progress === 1.0 && item.failed) status.failures++;
       }
       item = item.next;
     } while (isTask(item));
   }
   if (status.total > 0) {
-    status.ratio = status.finished / status.total;
+    status.progress = status.partial / status.total;
   }
   return Object.freeze(status);
 }
@@ -68,19 +96,20 @@ function progress(list) {
 function waitOn(list, thenable) {
   const task = increaseList(list);
   if (isTask(task)) {
-    const finish = function finish() {
-      task.finished = true;
-      task.awaiting = null;
-      Object.freeze(task);
-      notify(list, progress(list));
+    task.awaiting = Promise.resolve(thenable).then(
+      function() {
+        finish(task);
+      },
+      function() {
+        task.failed = true;
+        finish(task);
+      }
+    );
+    return function setProgress(value) {
+      update(task, value);
     };
-    task.awaiting = Promise.resolve(thenable).then(finish, function() {
-      task.failed = true;
-      finish();
-    });
-    return true;
   }
-  return false;
+  return null;
 }
 
 function addObserver(list, observer) {
@@ -100,9 +129,9 @@ function addObserver(list, observer) {
 function createStatus() {
   return Object.seal({
     total: 0,
-    finished: 0,
+    partial: 0.0,
+    progress: 0.0,
     failures: 0,
-    ratio: 0.0,
   });
 }
 
@@ -114,6 +143,10 @@ function isOfType(type, subject) {
   return (
     subject !== null && typeof subject === 'object' && subject[TYPE] === type
   );
+}
+
+function isValidProgress(value) {
+  return typeof value === 'number' && value >= 0.0 && value <= 1.0;
 }
 
 function notify(list, data) {
@@ -144,7 +177,9 @@ export {
   increaseList,
   isTask,
   isList,
-  progress,
+  update,
+  finish,
+  getOverallProgress,
   waitOn,
   addObserver,
 };
